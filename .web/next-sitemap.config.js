10db8cd114bd353433dfd8b5e26c85b19e7a0a53
Sakura <emanuel.beltran5853@alumnos.udg.mx>
2025-05-06 00:06:47 -0600

    Version 1.2.2 deployed

--- .web/next-sitemap.config.js
+++ .web/next-sitemap.config.js
@@ -1,1 +1,1 @@
-module.exports = {"siteUrl": "http://localhost:3000", "generateRobotsTxt": true}
+module.exports = {"siteUrl": "https://paginaweb-gray-ring.reflex.run", "generateRobotsTxt": true, "outDir": "_static"}
