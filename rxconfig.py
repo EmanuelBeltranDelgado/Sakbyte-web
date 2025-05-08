959746dbfdbc7354ceb3ab78fa9d4b18e8292b74
Sakura <emanuel.beltran5853@alumnos.udg.mx>
2025-05-07 19:53:40 -0600

    Version 1.2.4 deployed

--- rxconfig.py
+++ rxconfig.py
@@ -2,4 +2,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="PaginaWeb",
+    api_url="https://sakura-blossom-dev.up.railway.app:8000",
+    cors_allowed_origins=[
+        "http://localhost:3000/"
+        "https://sakura-blossom-dev-web.vercel.app/"
+    ],
 )
